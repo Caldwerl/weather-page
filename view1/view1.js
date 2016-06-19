5a39b8ce6437f0f6c61ce9b0a2642c6ecb5dde3a
Brian Caldwell <caldwerl@gmail.com>
2016-06-19 01:53:18 -0400

    Add functionality to switch between C and F temps

--- view1/view1.js
+++ view1/view1.js
@@ -11,12 +11,27 @@ angular.module('weather-page.view1', ['ngRoute'])
     query = res.data;
   });
 
+  var convertCtoF = function (tempInC) {
+    return (tempInC * 9 / 5) + 32;
+  };
+
   $scope.search = function () {
     $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + $scope.searchInput.trim() + '&APPID=' + query.key + '&units=metric')
       .then(function (res) {
         $scope.cityWeatherData = res.data;
+
+        $scope.cityWeatherData.celsius = true;
+        $scope.cityWeatherData.tempC = $scope.cityWeatherData.main.temp;
+        $scope.cityWeatherData.tempF = convertCtoF($scope.cityWeatherData.main.temp);
+
         $scope.searchComplete = true;
     });
+  };
+
+  $scope.switchCF = function () {
+    $scope.cityWeatherData.main.temp = $scope.cityWeatherData.celsius ? $scope.cityWeatherData.tempF : $scope.cityWeatherData.tempC;
+
+    $scope.cityWeatherData.celsius = !$scope.cityWeatherData.celsius;
   };
 
 
