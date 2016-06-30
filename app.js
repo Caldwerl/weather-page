013991222c571ed47647a2f49f3c5c2018b2d67c
Brian Caldwell <caldwerl@gmail.com>
2016-06-29 22:30:36 -0400

    Add temp conversion, timestamp

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 'use strict';
 
-var Button = ReactBootstrap.Button;
-
 var Glyphicon = ReactBootstrap.Glyphicon;
+
+var Button = ReactBootstrap.Button;
+var ButtonGroup = ReactBootstrap.ButtonGroup;
 
 var Panel = ReactBootstrap.Panel;
 var Media = ReactBootstrap.Media;
@@ -23,6 +24,40 @@ var WeatherSearchForm = React.createClass({
     getInitialState: function() {
         return {data: {}, weatherSearchInput: '', searchComplete: false};
     },
+    convertCtoF: function (celsius) {
+        return (celsius * 9 / 5) + 32;
+    },
+    setTemps: function (data) {
+        data.tempScale = 'C';
+
+        data.main.tempF = this.convertCtoF(data.main.temp).toFixed(1);
+        data.main.temp = data.main.temp.toFixed(1);
+        data.main.tempC = data.main.temp;
+
+        data.main.temp_maxF = this.convertCtoF(data.main.temp_max).toFixed(1);
+        data.main.temp_max = data.main.temp_max.toFixed(1);
+        data.main.temp_maxC = data.main.temp_max;
+
+        data.main.temp_minF = this.convertCtoF(data.main.temp_min).toFixed(1);
+        data.main.temp_min = data.main.temp_min.toFixed(1);
+        data.main.temp_minC = data.main.temp_min;
+        return data;
+    },
+    switchTemps: function (data) {
+        if (data.tempScale === 'C') {
+            data.main.temp = data.main.tempF
+            data.main.temp_max = data.main.temp_maxF
+            data.main.temp_min = data.main.temp_minF
+        } else {
+            data.main.temp = data.main.tempC
+            data.main.temp_max = data.main.temp_maxC
+            data.main.temp_min = data.main.temp_minC
+        }
+
+        data.tempScale = data.tempScale === 'C' ? 'F' : 'C';
+
+        this.setState({data: data});
+    },
     handleInputChange: function (e) {
         this.setState({weatherSearchInput: e.target.value});
     },
@@ -38,7 +73,9 @@ var WeatherSearchForm = React.createClass({
             })
             .then(function (json) {
                 console.log('CALL RESPONSE', json);
-                json.tempScale = 'C';
+                json = this.setTemps(json);
+                json.timeStamp = moment().format('LLL');
+                json.wind.deg = json.wind.deg ? json.wind.deg.toFixed(0) : 0;
                 this.setState({data: json, searchComplete: true});
             }.bind(this));
     },
@@ -66,7 +103,7 @@ var WeatherSearchForm = React.createClass({
                     </Col>
                 </Row>
                 <Row>
-                    {this.state.searchComplete ? <WeatherPanel data={this.state.data} /> : null}
+                    {this.state.searchComplete ? <WeatherPanel data={this.state.data} switchTemps={this.switchTemps} /> : null}
                 </Row>
             </div>
         );
@@ -74,10 +111,10 @@ var WeatherSearchForm = React.createClass({
 });
 
 var WeatherPanel = React.createClass({
-    header: function () {
+    panelHeader: function () {
         return (
             <Row>
-                <Col sm={8} xs={12}>
+                <Col sm={8}>
                     <Media>
                         <Media.Left>
                             <img src={'http://openweathermap.org/img/w/' + this.props.data.weather[0].icon + '.png'} responsive={true} />
@@ -87,16 +124,30 @@ var WeatherPanel = React.createClass({
                         </Media.Body>
                     </Media>
                 </Col>
-                <Col sm={4} xs={12}>
+                <Col sm={4}>
                     <h3>{this.props.data.weather[0].description}</h3>
                 </Col>
             </Row>
         );
     },
-    render: function () {
-        console.log({transform: [{rotate: this.props.data.wind.deg + 'deg'}]});
+    panelFooter: function () {
         return (
-            <Panel header={this.header()}>
+            <Row>
+                <Col sm={10}>
+                    <h4>Weather data as of {this.props.data.timeStamp}</h4>
+                </Col>
+                <Col sm={2}>
+                    <ButtonGroup>
+                        <Button onClick={this.props.switchTemps.bind(null, this.props.data)} disabled={this.props.data.tempScale === 'C'}>C</Button>
+                        <Button onClick={this.props.switchTemps.bind(null, this.props.data)} disabled={this.props.data.tempScale === 'F'}>F</Button>
+                    </ButtonGroup>
+                </Col>
+            </Row>
+        );
+    },
+    render: function () {
+        return (
+            <Panel header={this.panelHeader()} footer={this.panelFooter()}>
                 <Col sm={6}>
                     <Row>
                         <Col sm={6}>
@@ -180,7 +231,7 @@ var WeatherPageHeader = React.createClass({
                     <h2>Empty Datacube</h2>
                 </Navbar.Header>
                 <Nav>
-                    <NavItem href="https://github.com/Caldwerl">
+                    <NavItem href="https://github.com/Caldwerl" target="_blank">
                         <h4>Github</h4>
                     </NavItem>
                 </Nav>
